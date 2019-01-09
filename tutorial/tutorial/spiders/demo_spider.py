0cd4b4caf0956826c9790ee30003d250ab52210a
jiacuiping <1546732785@qq.com>
2019-01-10 00:53:01 +0800

    第一个spider

--- tutorial/tutorial/spiders/demo_spider.py
+++ tutorial/tutorial/spiders/demo_spider.py
@@ -2,12 +2,18 @@ import scrapy
 
 class DemoSpider(scrapy.Spider):
     name = "demo"
-    allowed_domains = ["shicimingju.com"]
-    start_urls = [
-        "http://www.shicimingju.com/",
-    ]
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        authors = response.xpath('//a[@class="www-main-container"]/li/a/text()').extract()
-        for author in authors:
-            print(author.strip())
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
